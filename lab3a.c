69ee6451c250f3066e453962e8ec4472a320fcc5
lin-meister <matthewcaus@gmail.com>
2017-05-28 17:47:19 -0700

    Added superblock code

--- lab3a.c
+++ lab3a.c
@@ -27,7 +27,13 @@ void printSuperblock()
 {
   long long unsigned int blocksCount = (long long unsigned int) superBlock->s_blocks_count;
   long long unsigned int inodesCount = (long long unsigned int) superBlock->s_inodes_count;
-  fprintf(stdout, "SUPERBLOCK,%llu,%llu,%llu\n", blocksCount, inodesCount);
+  long long unsigned int blockSize_1 = (long long unsigned int) superBlock->s_log_block_size;
+  long long unsigned int blockSize = EXT2_MIN_BLOCK_SIZE << blockSize_1;
+  long long unsigned int inodeSize = (long long unsigned int) superBlock->s_inode_size;
+  long long unsigned int blocksPerGroup = (long long unsigned int) superBlock->s_blocks_per_group;
+  long long unsigned int inodesPerGroup = (long long unsigned int) superBlock->s_inodes_per_group;
+  long long unsigned int firstUnreservedInode = (long long unsigned int) superBlock->s_first_ino;
+  fprintf(stdout, "SUPERBLOCK,%llu,%llu,%llu,%llu,%llu,%llu,%llu\n", blocksCount, inodesCount, blockSize, inodeSize, blocksPerGroup, inodesPerGroup, firstUnreservedInode);
 }
 
 int
