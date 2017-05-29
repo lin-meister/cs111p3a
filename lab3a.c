e1fb2124f6db3e528644b4fd5bfc25c8141a6cd2
Nikhil Bhatia <nbhatia823@ucla.edu>
2017-05-28 17:30:53 -0700

    Second commit by N

--- lab3a.c
+++ lab3a.c
@@ -10,6 +10,8 @@
 #define BUF_SIZE 1024
 
 struct ext2_super_block * superBlock;
+struct ext2_group_desc * groupBlock;
+
 int fd;
 
 void freeMemory()
@@ -20,14 +22,28 @@ void freeMemory()
 void error(char* msg)
 {
   fprintf(stderr, "%s\n", msg);
-  exit(1);
+  exit(2);
 }
 
 void printSuperblock()
 {
   long long unsigned int blocksCount = (long long unsigned int) superBlock->s_blocks_count;
   long long unsigned int inodesCount = (long long unsigned int) superBlock->s_inodes_count;
-  fprintf(stdout, "SUPERBLOCK,%llu,%llu,%llu\n", blocksCount, inodesCount);
+  long long unsigned int blockSize_1 = (long long unsigned int) superBlock->s_log_block_size;
+  long long unsigned int blockSize = EXT2_MIN_BLOCK_SIZE << blockSize_1;
+  long long unsigned int inodeSize = (long long unsigned int) superBlock->s_inode_size;
+  long long unsigned int blocksPerGroup = (long long unsigned int) superBlock->s_blocks_per_group;
+  long long unsigned int inodesPerGroup = (long long unsigned int) superBlock->s_inodes_per_group;
+  long long unsigned int firstUnreservedInode = (long long unsigned int) superBlock->s_first_ino;
+  fprintf(stdout, "SUPERBLOCK,%llu,%llu,%llu,%llu,%llu,%llu,%llu\n", blocksCount, inodesCount, blockSize, inodeSize, blocksPerGroup, inodesPerGroup, firstUnreservedInode);
+}
+
+void printGroupBlock() {
+
+  int blockNum = 0;
+  long long unsigned int totalNumofBlocks =(long long unsigned int) groupBlock-> bg_block_bitmap;
+  fprintf(stdout, "GROUP,%d,%llu\n",blockNum,totalNumofBlocks);
+
 }
 
 int
@@ -43,5 +59,10 @@ main (int argc, char **argv)
   pread(fd, superBlock, BUF_SIZE, BUF_SIZE);
 
   printSuperblock();
+
+  groupBlock = (struct ext2_group_desc *) malloc (BUF_SIZE);
+  pread(fd, groupBlock, BUF_SIZE, BUF_SIZE);
+  printGroupBlock();
+  
   return 0;
 }
